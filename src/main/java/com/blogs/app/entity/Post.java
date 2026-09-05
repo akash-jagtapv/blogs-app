dbdcf0edfc4ca45b3af115d31357454ee2723357
akash-jagtapv <aj5958302@gmail.com>
2026-09-05 20:01:28 +0530

    Added Post Entity class

---
+++ src/main/java/com/blogs/app/entity/Post.java
@@ -0,0 +1,27 @@
+package com.blogs.app.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 5000)
+    private String body;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
