d1e60e32d9ef1520947ee3ad2c3c43b5a4f5591e
akash-jagtapv <aj5958302@gmail.com>
2026-09-05 20:06:15 +0530

    Added PostRepository Repository interface

---
+++ src/main/java/com/blogs/app/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.blogs.app.repository;
+
+import com.blogs.app.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
